555e0bc79f22fe3369dec9c2e27dff21a80297d8
Nadlecy <bruno.cleymand@gmail.com>
2022-11-14 15:15:31 +0100

    a

--- morpion.py
+++ morpion.py
@@ -2,7 +2,7 @@
 
 # On admet une fonction input() qui enregistre l'entree d'un joueur sous forme de str
 
-# On admet une fonction split() qui prend en parametre une chaine de caracteres et renvoie 
+# On admet une fonction split() qui prend en parametre une chaine de caracteres str et renvoie une liste separant le contenu de str a chaque espace
 
 # definir une fonction display(tab) qui prend en parametre un tableau tab contenant des tableaux et l'affiche ligne par ligne
 def display(tab):
@@ -20,13 +20,28 @@ def display(tab):
 ans = input("position ?").split()
 # assigner a la variable ansValid la valeur True
 # si ans contient exactement deux éléments
+"""analyse ligne"""
     # alors
     # si la valeur a l'index 0 de ans est egale a "top" ou "t"
-        # alors assigner a la variable a l'index 1 de ans la valeur 0
+        # alors assigner a la variable a l'index 0 de ans la valeur 0
     # sinon si la valeur a l'index 0 de ans est egale a "mid" ou "middle" ou "m"
-        # alors
+        # alors assigner a la variable a l'index 0 de ans la valeur 1
     # sinon si la valeur a l'index 0 de ans est egale a "bot" ou "bottom" ou "b"
-        #alors
+        # alors assigner a la variable a l'index 0 de ans la valeur 2
+    # sinon
+        # alors assigner a ansValid la valeur False
+"""analyse colonne"""
+    # si ansValid est egale a True
+        # si la valeur a l'index 1 de ans est egale a "left" ou "l"
+            # alors assigner a la variable a l'index 1 de ans la valeur 0
+        # sinon si la valeur a l'index 1 de ans est egale a "center" ou "c"
+            # alors assigner a la variable a l'index 1 de ans la valeur 1
+        # sinon si la valeur a l'index 1 de ans est egale a "right" ou "r"
+            # alors assigner a la variable a l'index 1 de ans la valeur 2
+        # sinon
+            # alors assigner a ansValid la valeur False
+""""""
+
 # sinon
     # alors assigner a la variable ansValid la valeur False
 # si ansValid est egale a False
@@ -38,7 +53,7 @@ ans = input("position ?").split()
 
 
 # definir une fonction morpion() qui ne prend aucun parametre
-    # assigner a la variable morp le tableau [["_","_","_"],["_","_","_"],["_","_","_"]]
+    # assigner a la variable morp le tableau [["_ ","_ ","_ "],["_ ","_ ","_ "],["_ ","_ ","_ "]]
 
 
 # Fin
