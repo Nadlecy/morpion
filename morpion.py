4c1886438e9a018f24d8dfd9f022b824d29b5e9a
Nadlecy <bruno.cleymand@gmail.com>
2022-11-15 13:59:33 +0100

    update mOOOOAR

--- morpion.py
+++ morpion.py
@@ -1,5 +1,7 @@
 # Debut
 
+# On admet une fonction random() qui retourne un integer aléatoire entre 0 et 2 inclus
+
 # On admet une fonction input() qui enregistre l'entree d'un joueur sous forme de str (avec optionellement un message faisant office question)
 
 # On admet une fonction split() qui prend en parametre une chaine de caracteres str et renvoie une liste separant le contenu de str a chaque espace
@@ -24,6 +26,7 @@ def display(tab):
     # assigner a la variable playerWin la valeur False
     # assigner a la variable comWin la valeur False
     # assigner a la variable choiceStartDone la valeur False
+"""JOUER EN PREMIER ?????"""
     # tant que choiceStartDone est egale a False
         # alors
         # assigner a la variable userStart le retour de la fonction input() avec le message "Voulez-vous jouer en premier ? y/n"
@@ -32,53 +35,71 @@ def display(tab):
         # sinon
             # alors afficher le message "Réponse invalide!"
 """DEBUT BOUCLE ROUNDS"""
-    # tant que 
-
-
-
-
-
-"""EVALUTAION ENTREE JOUEUR"""
-# assigner a la variable userTurnDone la valeur False
-# tant que userTurnDone est egale a False
-    # assigner a la variable ans le retour de la fonction split appliquee sur le retour de la fonction input avec le message "position ?"
-    # assigner a la variable ansValid la valeur True
-    # si ans contient exactement deux éléments
-"""analyse ligne"""
+    # tant que comWin est egale a False et playerWin est egale a False et numPlays est inferieure a 9
         # alors
-        # si la valeur a l'indice 0 de ans est egale a "top" ou "t"
-            # alors assigner a la variable a l'indice 0 de ans la valeur 0
-        # sinon si la valeur a l'indice 0 de ans est egale a "mid" ou "middle" ou "m"
-            # alors assigner a la variable a l'indice 0 de ans la valeur 1
-        # sinon si la valeur a l'indice 0 de ans est egale a "bot" ou "bottom" ou "b"
-            # alors assigner a la variable a l'indice 0 de ans la valeur 2
-        # sinon
-            # alors assigner a ansValid la valeur False
-"""analyse colonne"""
-        # si ansValid est egale a True
-            #alors
-            # si la valeur a l'indice 1 de ans est egale a "left" ou "l"
-                # alors assigner a la variable a l'indice 1 de ans la valeur 0
-            # sinon si la valeur a l'indice 1 de ans est egale a "center" ou "c"
-                # alors assigner a la variable a l'indice 1 de ans la valeur 1
-            # sinon si la valeur a l'indice 1 de ans est egale a "right" ou "r"
-                # alors assigner a la variable a l'indice 1 de ans la valeur 2
-            # sinon
-                # alors assigner a ansValid la valeur False
-"""CHANGEMENT DE PARTIE USER"""
-            # si ansValid est egale a True
-                #alors
-                # si la valeur de la variable dans morp aux indices u dans i (où i est la valeur dans ans à l'indice 0 et u est la valeur dans ans à l'indice 1) est egale a "_  "
-                    # alors assigner a la variable dans morp aux indices u dans i (où i est la valeur dans ans à l'indice 0 et u est la valeur dans ans à l'indice 1) la valeur "X  "
+"""ENTREE ORDI SI JOUE EN PREMIERIERIMLEILMEI"""
+        # si userStart est egale a "n":
+            # alors assigner a la variable comTurnDone la valeur False
+            # tant que comTurnDone est egale a False
+                # alors
+                # assigner a comAns un tableau contenant deux retours de la fonction random()
+                # si la valeur de la variable dans morp aux indices u dans i (où i est la valeur dans comAns à l'indice 0 et u est la valeur dans comAns à l'indice 1) est egale a "_  "
+                    # alors assigner a la variable dans morp aux indices u dans i (où i est la valeur dans comAns à l'indice 0 et u est la valeur dans comAns à l'indice 1) la valeur "O  "
                     # utiliser la fonction display() avec morp en parametre
                     # incrementer numPlays de 1
-                    # assigner a userTurnDone la valeur True
-                # sinon 
+                    # assigner a comTurnDone la valeur True
+"""EVALUTAION ENTREE JOUEUR"""
+        # assigner a la variable userTurnDone la valeur False
+        # tant que userTurnDone est egale a False
+            # assigner a la variable ans le retour de la fonction split appliquee sur le retour de la fonction input avec le message "position ?"
+            # assigner a la variable ansValid la valeur True
+            # si ans contient exactement deux éléments
+"""analyse ligne"""
+            # alors
+            # si la valeur a l'indice 0 de ans est egale a "top" ou "t"
+                # alors assigner a la variable a l'indice 0 de ans la valeur 0
+            # sinon si la valeur a l'indice 0 de ans est egale a "mid" ou "middle" ou "m"
+                # alors assigner a la variable a l'indice 0 de ans la valeur 1
+            # sinon si la valeur a l'indice 0 de ans est egale a "bot" ou "bottom" ou "b"
+                # alors assigner a la variable a l'indice 0 de ans la valeur 2
+            # sinon
+                # alors assigner a ansValid la valeur False
+"""analyse colonne"""
+            # si ansValid est egale a True
+                #alors
+                # si la valeur a l'indice 1 de ans est egale a "left" ou "l"
+                    # alors assigner a la variable a l'indice 1 de ans la valeur 0
+                # sinon si la valeur a l'indice 1 de ans est egale a "center" ou "c"
+                    # alors assigner a la variable a l'indice 1 de ans la valeur 1
+                # sinon si la valeur a l'indice 1 de ans est egale a "right" ou "r"
+                    # alors assigner a la variable a l'indice 1 de ans la valeur 2
+                # sinon
                     # alors assigner a ansValid la valeur False
-    # sinon
-        # alors assigner a la variable ansValid la valeur False
-    # si ansValid est egale a False
-        # alors afficher "Réponse Invalide !"
+"""CHANGEMENT DE PARTIE USER"""
+                # si ansValid est egale a True
+                    #alors
+                    # si la valeur de la variable dans morp aux indices u dans i (où i est la valeur dans ans à l'indice 0 et u est la valeur dans ans à l'indice 1) est egale a "_  "
+                        # alors assigner a la variable dans morp aux indices u dans i (où i est la valeur dans ans à l'indice 0 et u est la valeur dans ans à l'indice 1) la valeur "X  "
+                        # utiliser la fonction display() avec morp en parametre
+                        # incrementer numPlays de 1
+                        # assigner a userTurnDone la valeur True
+                    # sinon 
+                        # alors assigner a ansValid la valeur False
+        # sinon
+            # alors assigner a la variable ansValid la valeur False
+        # si ansValid est egale a False
+            # alors afficher "Réponse Invalide !"
+"""ENTREE ORDI SI JOUE EN PREMIERIERIMLEILMEI"""
+        # si userStart est egale a "y":
+            # alors assigner a la variable comTurnDone la valeur False
+            # tant que comTurnDone est egale a False
+                # alors
+                # assigner a comAns un tableau contenant deux retours de la fonction random()
+                # si la valeur de la variable dans morp aux indices u dans i (où i est la valeur dans comAns à l'indice 0 et u est la valeur dans comAns à l'indice 1) est egale a "_  "
+                    # alors assigner a la variable dans morp aux indices u dans i (où i est la valeur dans comAns à l'indice 0 et u est la valeur dans comAns à l'indice 1) la valeur "O  "
+                    # utiliser la fonction display() avec morp en parametre
+                    # incrementer numPlays de 1
+                    # assigner a comTurnDone la valeur True
 
 
 # Fin
